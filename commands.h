6db68a51d98277817f1707e55ece02bedf806aac
Mark McConachie <mark@markmcconachie.com>
2013-10-29 02:49:55 +0100

    Change defined constants to enum

--- commands.h
+++ commands.h
@@ -5,8 +5,10 @@
 #define MAX_HOST_LENGTH 128
 #define MAX_LINES 1024
 
-#define KH_NOT_FOUND 0
-#define KH_FOUND 1
+enum found_states {
+  KH_NOT_FOUND,
+  KH_FOUND
+};
 
 void list_hosts();
 void rm_host(char *hostToRm);
