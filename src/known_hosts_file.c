a2f2d790eb16532429337b4b90a0c71b360bdd46
Mark McConachie <mark@mcconachie.me>
2016-03-25 01:54:48 +0100

    Update formatting

--- src/known_hosts_file.c
+++ src/known_hosts_file.c
@@ -4,14 +4,14 @@
 #include "known_hosts_file.h"
 #include "config.h"
 
-static char* get_file_path() {
+static char *get_file_path() {
     static char localfilename[80];
-    strncpy(localfilename,"", sizeof(char));
+    strncpy(localfilename, "", sizeof(char));
     strcat(localfilename, getenv("HOME"));
     strcat(localfilename, KnownHosts_FILE_LOCATION);
     return localfilename;
 }
 
-FILE* known_hosts_file_get(char *mode) {
+FILE *known_hosts_file_get(char *mode) {
     return fopen(get_file_path(), mode);
 }
--- src/main.c
+++ src/main.c
@@ -5,21 +5,21 @@
 #include "commands.h"
 #include "display.h"
 
-int arg_matches(char* arg, char* command) {
+int arg_matches(char *arg, char *command) {
     return strcmp(arg, command) == 0;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
     if (argc < 2) display_usage(NULL);
 
     if (arg_matches(argv[1], "ls")) {
         commands_ls_hosts();
-    } else if(arg_matches(argv[1], "rm")) {
+    } else if (arg_matches(argv[1], "rm")) {
         if (argc != 3) display_usage("Host required");
         commands_rm_host(argv[2]);
-    } else if(arg_matches(argv[1], "version")) {
+    } else if (arg_matches(argv[1], "version")) {
         printf("Version "KnownHosts_VERSION "\n");
-    } else if(arg_matches(argv[1], "help")) {
+    } else if (arg_matches(argv[1], "help")) {
         display_usage(NULL);
     } else {
         display_usage(argv[1]);
